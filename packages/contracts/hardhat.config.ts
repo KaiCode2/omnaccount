c8e9c6c6433a290a31a448343186ebad3d94a9d6
Draimonox <draimonox@gmail.com>
2024-02-29 19:29:38 -0700

    safe:send task

--- packages/contracts/hardhat.config.ts
+++ packages/contracts/hardhat.config.ts
@@ -7,7 +7,7 @@ import "@nomicfoundation/hardhat-toolbox";
 import "@nomicfoundation/hardhat-verify";
 import "hardhat-deploy";
 import "tsconfig-paths/register";
-import { getSingletonFactoryInfo } from '@safe-global/safe-singleton-factory'
+import { getSingletonFactoryInfo } from "@safe-global/safe-singleton-factory";
 
 dotenv.config();
 
@@ -17,34 +17,38 @@ import { DeterministicDeploymentInfo } from "hardhat-deploy/types";
 const mnemonic =
   process.env.MNEMONIC ??
   process.env.DEPLOYER_MNEMONIC ??
-  'tattoo clip ankle prefer cruise car motion borrow bread future legal system'
+  "tattoo clip ankle prefer cruise car motion borrow bread future legal system";
 export const accounts = {
   mnemonic: mnemonic,
   path: "m/44'/60'/0'/0",
   initialIndex: 0,
   count: 10,
-  passphrase: '',
-}
+  passphrase: "",
+};
 
 const polygonRpcUrl =
   process.env.POLYGON_RPC_URL ??
-  (process.env.INFURA_API_KEY ? `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}` : undefined) ??
+  (process.env.INFURA_API_KEY
+    ? `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`
+    : undefined) ??
   (process.env.ALCHEMY_API_KEY
     ? `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
     : undefined) ??
-  "https://polygon-pokt.nodies.app"
+  "https://polygon-pokt.nodies.app";
 
 const mumbaiRpcUrl =
   process.env.MUMBAI_RPC_URL ??
-  (process.env.INFURA_API_KEY ? `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}` : undefined) ??
+  (process.env.INFURA_API_KEY
+    ? `https://polygon-mumbai.infura.io/v3/${process.env.INFURA_API_KEY}`
+    : undefined) ??
   (process.env.ALCHEMY_API_KEY
     ? `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
     : undefined) ??
-  "https://polygon-mumbai-pokt.nodies.app"
+  "https://polygon-mumbai-pokt.nodies.app";
 
 const sepoliaRpcUrl = `https://eth-sepolia.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`;
-const arbSepoliaRpcUrl = "https://arbitrum-sepolia.blockpi.network/v1/rpc/public";
-
+const arbSepoliaRpcUrl =
+  "https://arbitrum-sepolia.blockpi.network/v1/rpc/public";
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -73,6 +77,9 @@ const config: HardhatUserConfig = {
       saveDeployments: true,
       autoImpersonate: true,
       deploy: ["deploy/local"],
+      // forking: {
+      //   url: sepoliaRpcUrl,
+      // },
     },
     localhost: {
       url: "http://127.0.0.1:8545",
@@ -110,7 +117,7 @@ const config: HardhatUserConfig = {
     owner: 0,
     deployer: 1,
     spokePool: {
-      default: 9, // TODO: Update
+      default: "0x5ef6C01E11889d86803e0B23e3cB3F9E9d97B662", // TODO: Update
       sepolia: "0x5ef6C01E11889d86803e0B23e3cB3F9E9d97B662",
       arbitrumSepolia: "0x7E63A5f1a8F0B4d0934B2f2327DAED3F6bb2ee75",
     },
@@ -120,7 +127,7 @@ const config: HardhatUserConfig = {
     WETH: {
       sepolia: "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14",
       // sepoliaBase: "",
-    }
+    },
   },
   deterministicDeployment,
   typechain: {
@@ -130,23 +137,23 @@ const config: HardhatUserConfig = {
 };
 
 function deterministicDeployment(network: string): DeterministicDeploymentInfo {
-  const info = getSingletonFactoryInfo(parseInt(network))
+  const info = getSingletonFactoryInfo(parseInt(network));
   if (!info) {
     throw new Error(`
       Safe factory not found for network ${network}. You can request a new deployment at https://github.com/safe-global/safe-singleton-factory.
       For more information, see https://github.com/safe-global/safe-contracts#replay-protection-eip-155
-    `)
+    `);
   }
 
-  const gasLimit = BigInt(info.gasLimit)
-  const gasPrice = BigInt(info.gasPrice)
+  const gasLimit = BigInt(info.gasLimit);
+  const gasPrice = BigInt(info.gasPrice);
 
   return {
     factory: info.address,
     deployer: info.signerAddress,
     funding: String(gasLimit * gasPrice),
     signedTx: info.transaction,
-  }
+  };
 }
 
 export default config;
--- packages/contracts/tasks/safe.ts
+++ packages/contracts/tasks/safe.ts
@@ -11,9 +11,17 @@ import deploySafeProxy, {
 import Safe from "@safe-global/safe-contracts/build/artifacts/contracts/Safe.sol/Safe.json";
 import SafeProxyFactory from "@safe-global/safe-contracts/build/artifacts/contracts/proxies/SafeProxyFactory.sol/SafeProxyFactory.json";
 import type { HardhatRuntimeEnvironment, TaskArguments } from "hardhat/types";
-import { getCreate2Address, keccak256, ZeroHash } from "ethers";
+import {
+  ethers,
+  getAddress,
+  getCreate2Address,
+  keccak256,
+  ZeroHash,
+} from "ethers";
 import { ISafe__factory, OmnaccountModule__factory } from "@/typechain-types";
 import execSafeTransaction from "@/deploy/utils/exec_transaction";
+import { SignerWithAddress } from "@nomicfoundation/hardhat-ethers/signers";
+import { AcrossSender__factory } from "@/typechain-types";
 
 task("safe:make", "Makes a new safe")
   .addOptionalParam("signer", "Signer of the new safe")
@@ -108,14 +116,10 @@ task("safe:sign", "Signs a safe transaction")
       );
 
       const safe = ISafe__factory.connect(safeAddress, ownerSigner);
-      const address = await safe.getAddress()
-      const nonce = await safe.nonce()
+      const address = await safe.getAddress();
+      const nonce = await safe.nonce();
 
-      const tx = await execSafeTransaction(
-        safe,
-        args,
-        ownerSigner
-      );
+      const tx = await execSafeTransaction(safe, args, ownerSigner);
 
       console.log(`Transaction sent: ${tx.hash}`);
 
@@ -158,3 +162,105 @@ async function getAddresses(
     omnaccountModuleAddress,
   };
 }
+// Get address for deployed safe if exsists, if it doesnt exsist revert
+// encode safe exec module transaction for bridge one ( on across sender.sol [call it])
+// sign module transaction then submit
+task("safe:send", "Sends a transaction across chains")
+  .addPositionalParam("tokenIn", "Address of the token to send")
+  .addPositionalParam("tokenOut", "Address of the token to receive")
+  .addPositionalParam("amountIn", "Amount of tokenIn to send")
+  .addPositionalParam("amountOut", "Expected amount of tokenOut to receive")
+  .addPositionalParam("destinationChainId", "Destination chain ID")
+  .addOptionalParam("message", "Additional message data")
+  .setAction(
+    async (
+      {
+        tokenIn,
+        tokenOut,
+        amountIn,
+        amountOut,
+        destinationChainId,
+        message,
+      }: TaskArguments,
+      { getNamedAccounts, ethers }: HardhatRuntimeEnvironment
+    ) => {
+      const { spokePool, entrypoint, owner } = await getNamedAccounts();
+      const ownerSigner = await ethers.getSigner(owner);
+
+      const amountInBN = ethers.getBigInt(amountIn);
+      const amountOutBN = ethers.getBigInt(amountOut);
+
+      console.log("Token In:", tokenIn);
+      console.log("Token Out:", tokenOut);
+      console.log("Amount In:", amountInBN.toString());
+      console.log("Amount Out:", amountOutBN.toString());
+      console.log("Destination Chain ID:", destinationChainId);
+      console.log("Message:", message);
+
+      const { chainId } = await ownerSigner.provider.getNetwork();
+      const { address: factoryAddress } = getSingletonFactoryInfo(
+        Number(chainId)
+      ) as SingletonFactoryInfo;
+
+      const {
+        safeMastercopyAddress,
+        safeProxyFactoryAddress,
+        omnaccountModuleAddress,
+      } = await getAddresses(factoryAddress, entrypoint, spokePool);
+
+      const safeAddress = calculateProxyAddress(
+        calculateInitializer(owner),
+        safeProxyFactoryAddress,
+        safeMastercopyAddress
+      );
+
+      let safe;
+      try {
+        const deployedVault = await ownerSigner.provider.getCode(safeAddress);
+        if (deployedVault === "0x") {
+          throw new Error("Vault not deployed");
+        }
+
+        safe = ISafe__factory.connect(safeAddress, ownerSigner);
+      } catch (e: any) {
+        console.error(e.message);
+        throw new Error("Vault not deployed");
+      }
+
+      const encodedData =
+        AcrossSender__factory.createInterface().encodeFunctionData(
+          "sendAcrossTransaction",
+          [
+            tokenIn,
+            tokenOut,
+            amountInBN,
+            amountOutBN,
+            destinationChainId,
+            ethers.hexlify(message || []),
+          ]
+        );
+
+      console.log("Encoded data:", encodedData);
+
+      const tx = await execSafeTransaction(
+        safe,
+        {
+          to: safeAddress,
+          data: encodedData,
+          value: 0,
+        },
+        ownerSigner
+      );
+
+      console.log(`Transaction sent: ${tx.hash}`);
+
+      const receipt = await tx.wait();
+
+      if (receipt && receipt.status === 0) {
+        console.error(`Transaction failed: ${tx.hash}`);
+        return;
+      } else {
+        console.log(`Transaction confirmed: ${tx.hash}`);
+      }
+    }
+  );
